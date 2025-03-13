9555fa44f54aa0e7c6888cf265c7f1c70190766c
Flash31_ <arterentev128@gmail.com>
2025-03-14 01:21:35 +0300

    Start

---
+++ Assets/_Burger-YandexGame/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    private Rigidbody _rb;
+
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        //float vertical = Input.GetAxis("Vertical");
+        float vertical = 1;
+
+        Vector3 movement = new Vector3(horizontal, 0f, vertical);
+
+        Vector3 newPosition = _rb.position + movement * _speed * Time.fixedDeltaTime;
+
+        _rb.MovePosition(newPosition);
+    }
+}
