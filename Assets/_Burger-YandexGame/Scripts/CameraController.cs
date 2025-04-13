e14305ce8292d35d570386a986fec0cc119368ce
Flash31_ <arterentev128@gmail.com>
2025-04-13 12:56:52 +0300

    Update Rotation

--- Assets/_Burger-YandexGame/Scripts/CameraController.cs
+++ Assets/_Burger-YandexGame/Scripts/CameraController.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
@@ -5,18 +6,29 @@ public class CameraController : MonoBehaviour
     [SerializeField] private Transform _player;
     [SerializeField] private Vector3 _offset;
 
+    [SerializeField] private float rotationTweenDuration = 0.5f;
+
+    private Quaternion _rotationOffset = Quaternion.identity;
+
+    private Tween rotationTween;
+
     private void Start()
     {
         _offset = transform.localPosition;
+        _rotationOffset = transform.rotation;
     }
 
     void LateUpdate()
     {
         if(GameManager.Instance.Player.CanMove)
         {
-            Vector3 targetPosition = _player.position + _offset;
-
+            Vector3 targetPosition = _player.position + _player.rotation * _offset;
             transform.position = targetPosition;
+
+            Quaternion targetRotation = _player.rotation * _rotationOffset;
+
+            float rotationSmoothSpeed = 5f; // Можно настроить для более медленного или быстрого сглаживания
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * rotationSmoothSpeed);
         }
     }
 }
--- Assets/_Burger-YandexGame/Scripts/Scene/RotationTrigger.cs
+++ Assets/_Burger-YandexGame/Scripts/Scene/RotationTrigger.cs
@@ -4,19 +4,21 @@ using UnityEngine;
 public class RotationTrigger : MonoBehaviour
 {
     [SerializeField] private float _speed;
-    [SerializeField] private GameObject _closeWall;
+    [SerializeField] private GameObject _newRoadWall;
+    [SerializeField] private GameObject _oldRoadWall;
     [SerializeField] private Vector3 _toRotate;
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.TryGetComponent(out Player player))
         {
-            player.Vertical = 0;
+            player.Vertical = 0.4f;
 
             player.transform.DORotate(_toRotate, _speed).OnComplete(() =>
             {
                 player.Vertical = 1;
-                _closeWall.SetActive(false);
+                _newRoadWall.SetActive(false);
+                _oldRoadWall.SetActive(true);
             });
         }
     }
